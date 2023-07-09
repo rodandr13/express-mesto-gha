987da358f2b713b671a2a2e7d4cc8b7a370592ed
Andrey Roditelev <rodandr13@gmail.com>
2023-07-09 20:21:45 +0300

    fead: add express and mongoose

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const { PORT = 3000 } = process.env;
+
+mongoose.connect('mongodb://localhost:27017/mestodb');
+
+app.listen(PORT, () => {
+  console.log('сервер запущен');
+});
