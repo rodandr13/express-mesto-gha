921b1c1f8adabd8e8550f1e6465bb5393edd9f72
Andrey Roditelev <rodandr13@gmail.com>
2023-07-12 00:38:40 +0300

    feat: add patch for users

--- controllers/users.js
+++ controllers/users.js
@@ -33,8 +33,34 @@ const getUser = (req, res) => {
     });
 };
 
+const updateUser = (req, res) => {
+  const { name, about } = req.body;
+  const userId = req.user._id;
+  User.findByIdAndUpdate(userId, { name, about }, { new: true, runValidators: true })
+    .then((user) => {
+      res.send(user);
+    })
+    .catch((error) => {
+      res.status(400).send(error);
+    });
+};
+
+const updateAvatar = (req, res) => {
+  const { avatar } = req.body;
+  const userId = req.user._id;
+  User.findByIdAndUpdate(userId, { avatar }, { new: true, runValidators: true })
+    .then((user) => {
+      res.send(user);
+    })
+    .catch((error) => {
+      res.status(400).send(error);
+    });
+};
+
 module.exports = {
   createUser,
   getUsers,
   getUser,
+  updateUser,
+  updateAvatar,
 };
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,16 @@
 const router = require('express').Router();
-const { createUser, getUsers, getUser } = require('../controllers/users');
+const {
+  createUser,
+  getUsers,
+  getUser,
+  updateUser,
+  updateAvatar,
+} = require('../controllers/users');
 
 router.post('/', createUser);
 router.get('/', getUsers);
 router.get('/:id', getUser);
+router.patch('/me', updateUser);
+router.patch('/me/avatar', updateAvatar);
 
 module.exports = router;
