9d21961e47d890640703fcc9bf4f747f60d30144
Andrey Roditelev <rodandr13@gmail.com>
2023-07-11 16:05:59 +0300

    feat: add user router and controller

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const usersRouter = require('./routes/users');
 
 const app = express();
 const { PORT = 3000 } = process.env;
 
-mongoose.connect('mongodb://localhost:27017/mestodb');
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
+app.use(bodyParser.json());
+app.use('/users', usersRouter);
 app.listen(PORT, () => {
   console.log('сервер запущен');
 });
---
+++ routes/users.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+const { createUser, getUsers, getUser } = require('../controllers/users');
+
+router.post('/', createUser);
+router.get('/', getUsers);
+router.get('/:id', getUser);
+
+module.exports = router;
