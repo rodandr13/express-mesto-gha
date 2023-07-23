49f6a1845c06b60ca58c4d2d521384e702f6507d
Andrey Roditelev <rodandr13@gmail.com>
2023-07-23 23:41:10 +0300

    feat: add joi validation for crd

--- routes/cards.js
+++ routes/cards.js
@@ -9,10 +9,45 @@ const {
   dislikeCard,
 } = require('../controllers/cards');
 
-router.post('/', createCard);
+router.post(
+  '/',
+  celebrate({
+    params: Joi.object().keys({
+      name: Joi.string().min(2).max(30),
+      link: Joi.string().regex(
+        /^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)$/i,
+      ),
+    }),
+  }),
+  createCard,
+);
 router.get('/', getCards);
-router.delete('/:cardId', deleteCard);
-router.put('/:cardId/likes', likeCard);
-router.delete('/:cardId/likes', dislikeCard);
+router.delete(
+  '/:cardId',
+  celebrate({
+    params: Joi.object().keys({
+      id: Joi.string().required().length(24).hex(),
+    }),
+  }),
+  deleteCard,
+);
+router.put(
+  '/:cardId/likes',
+  celebrate({
+    params: Joi.object().keys({
+      id: Joi.string().required().length(24).hex(),
+    }),
+  }),
+  likeCard,
+);
+router.delete(
+  '/:cardId/likes',
+  celebrate({
+    params: Joi.object().keys({
+      id: Joi.string().required().length(24).hex(),
+    }),
+  }),
+  dislikeCard,
+);
 
 module.exports = router;
