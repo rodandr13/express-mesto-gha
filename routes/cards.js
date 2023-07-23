ffeb27bcf07f865ff81e218d4488bbf5c6411817
Andrey Roditelev <rodandr13@gmail.com>
2023-07-23 23:48:27 +0300

    fix: card validation

--- routes/cards.js
+++ routes/cards.js
@@ -13,8 +13,8 @@ router.post(
   '/',
   celebrate({
     params: Joi.object().keys({
-      name: Joi.string().min(2).max(30),
-      link: Joi.string().regex(
+      name: Joi.string().required().min(2).max(30),
+      link: Joi.string().required().regex(
         /^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)$/i,
       ),
     }),
