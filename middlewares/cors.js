f65978eed9ba891985061e00056240499bb215bc
Andrey Roditelev <rodandr13@gmail.com>
2023-07-30 23:32:37 +0300

    fix: eslint

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -12,7 +12,7 @@ const cors = (req, res, next) => {
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
-    return res.end();
+    res.end();
   }
 
   next();
